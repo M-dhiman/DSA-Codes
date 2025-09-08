fc756c24821a039ffbde87806902965a1782b254
Mukul Dhiman <mukuldhiman72182@gmail.com>
2025-09-08 09:43:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (58.36%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.c
@@ -0,0 +1,27 @@
+int search(int nums[], int n, int target) {
+    int s = 0, e = n - 1;
+
+    while (s <= e) {
+        int mid = s + (e - s) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[s] <= nums[mid]) {
+            if (nums[s] <= target && target <= nums[mid]) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        } else {
+            if (nums[mid] <= target && target <= nums[e]) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
